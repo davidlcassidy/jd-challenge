196d4410a57f46d7eb142ddb48e5cd3628e9a0ca
davidlcassidy <github@davidlcassidy.com>
2024-01-18 17:46:38 +0100

    Create Spring Boot skeleton

---
+++ src/main/java/com/davidlcassidy/jdchallenge/JohnDeereCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.davidlcassidy.jdchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnDeereCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnDeereCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidlcassidy/jdchallenge/JohnDeereCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidlcassidy.jdchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnDeereCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
