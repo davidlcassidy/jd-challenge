8fd03386eb6e511b958b86a2c217e5264bea06bd
davidlcassidy <github@davidlcassidy.com>
2024-01-24 00:05:14 +0100

    Swagger documentation cleanup

--- src/main/java/com/davidlcassidy/jdchallenge/config/SwaggerConfig.java
+++ src/main/java/com/davidlcassidy/jdchallenge/config/SwaggerConfig.java
@@ -13,13 +13,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("SpringShop API")
-                        .description("Spring shop sample application")
+                .info(new Info().title("John Deere Challenge API")
+                        .description("Create by David Cassidy")
                         .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("SpringShop Wiki Documentation")
-                        .url("https://springshop.wiki.github.org/docs"));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
-
 }
